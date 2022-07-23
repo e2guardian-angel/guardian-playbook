08c4b2b6ef9b36705f5a40c097e912dc879e9f93
Justin Schwartzbeck <jusschwa@debian-dev>
2022-07-23 00:51:45 -0500

    no become ask pass

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 apt-get install -y ansible
 
-ansible-playbook --connection=local --inventory 127.0.0.1 --limit 127.0.0.1 site.yml -K
+ansible-playbook --connection=local --inventory 127.0.0.1 --limit 127.0.0.1 site.yml
 
