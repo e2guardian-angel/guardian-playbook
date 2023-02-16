6766c8effb1d27657ca63e9c97960421b884be91
Justin Schwartzbeck <justinmschw@gmail.com>
2023-02-16 16:31:15 -0600

    reboot on cmdline.txt change

--- setup.sh
+++ setup.sh
@@ -2,5 +2,14 @@
 
 apt-get install -y ansible
 
+CMDLINE_BEFORE=$(shasum -a 256 /boot/cmdline.txt | cut -d' ' -f1)
 ansible-playbook --connection=local -i hosts.yml -e "@extra.yml" setup.yml
+CMDLINE_AFTER=$(shasum -a 256 /boot/cmdline.txt | cut -d' ' -f1)
 
+if [ "$CMDLINE_BEFORE" == "$CMDLINE_AFTER" ]; then
+    echo "cmdline.txt not changed; no need to reboot"
+else
+    echo "cmdline.txt changed, rebooting..."
+    reboot
+fi
+  
