9f4b2a5ba916b2d3752ac4c2ec0cb8bb84f9d9f6
Justin Schwartzbeck <justinmschw@gmail.com>
2023-02-17 13:09:01 -0600

    install pip

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y ansible
+apt-get install -y ansible python3-pip
 pip3 install openshift pyyaml kubernetes --user
 ansible-galaxy collection install kubernetes.core
 ansible-playbook --connection=local -i hosts.yml -e "@extra.yml" setup.yml
