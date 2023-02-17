eb5792f1f5c3dd0918c851299d64bedd2a4812d2
Justin Schwartzbeck <justinmschw@gmail.com>
2023-02-17 13:07:20 -0600

    install kube python libs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 apt-get install -y ansible
+pip3 install openshift pyyaml kubernetes --user
 ansible-galaxy collection install kubernetes.core
 ansible-playbook --connection=local -i hosts.yml -e "@extra.yml" setup.yml
