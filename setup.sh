054089e985206f2a3b4f1143c58b38eb335c5d22
Justin Schwartzbeck <justinmschw@gmail.com>
2023-02-17 13:11:07 -0600

    noninteractive

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y ansible python3-pip
+DEBIAN_FRONTEND=noninteractive apt-get install -y ansible python3-pip
 pip3 install openshift pyyaml kubernetes --user
 ansible-galaxy collection install kubernetes.core
 ansible-playbook --connection=local -i hosts.yml -e "@extra.yml" setup.yml
